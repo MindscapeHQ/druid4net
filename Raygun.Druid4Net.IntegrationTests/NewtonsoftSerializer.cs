85bd09ec2e595e7d21a849bbfc031bcef7f0a487
Jeremy Norman <jnorman@raygun.io>
2018-12-15 22:42:20 +1300

    Update package metadata and docs

---
+++ Raygun.Druid4Net.IntegrationTests/NewtonsoftSerializer.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+
+namespace Raygun.Druid4Net.IntegrationTests
+{
+  public class NewtonsoftSerializer : IJsonSerializer
+  {
+    private readonly JsonSerializerSettings _settings = new JsonSerializerSettings
+    {
+      ContractResolver = new CamelCasePropertyNamesContractResolver(),
+      NullValueHandling = NullValueHandling.Ignore,
+      Converters = new List<JsonConverter>
+      {
+        new Newtonsoft.Json.Converters.StringEnumConverter()
+      }
+    };
+    
+    public string Serialize<TRequest>(TRequest request)
+    {
+      return JsonConvert.SerializeObject(request, _settings);
+    }
+
+    public TResponse Deserialize<TResponse>(string responseData)
+    {
+      return JsonConvert.DeserializeObject<TResponse>(responseData, _settings);
+    }
+  }
+}
