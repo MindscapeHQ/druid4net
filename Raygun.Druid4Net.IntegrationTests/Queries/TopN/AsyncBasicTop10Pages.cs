fa5355427d1bb6e7e83d53b61ba45ea22f180dff
Jeremy Norman <jnorman@raygun.io>
2019-01-02 14:09:31 +1000

    Add async to docs and async integration test example

---
+++ Raygun.Druid4Net.IntegrationTests/Queries/TopN/AsyncBasicTop10Pages.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Raygun.Druid4Net.IntegrationTests.Queries.TopN
+{
+  [TestFixture]
+  public class AsyncBasicTop10Pages : TestQueryBase
+  {
+    private IList<QueryResult> _results;
+
+    [SetUp]
+    public async Task Execute()
+    {
+      var response = await DruidClient.TopNAsync<QueryResult>(q => q
+        .Metric(Wikiticker.Metrics.Count)
+        .Dimension(Wikiticker.Dimensions.Page)
+        .Threshold(10)
+        .Aggregations(new LongSumAggregator(Wikiticker.Metrics.Count))
+        .DataSource(Wikiticker.DataSource)
+        .Interval(FromDate, ToDate)
+        .Granularity(Granularities.All)  
+      );
+
+      _results = response.Data.SelectMany(x => x.Result).ToList();
+    }
+
+    [Test]
+    public void QueryHasCorrectNumberOfResults()
+    {
+      Assert.That(_results.Count, Is.EqualTo(10));
+    }
+
+    [Test]
+    public void FirstResultIsCorrect()
+    {
+      Assert.That(_results.First().Page, Is.EqualTo("Wikipedia:Vandalismusmeldung"));
+      Assert.That(_results.First().Count, Is.EqualTo(33));
+    }
+
+    [Test]
+    public void LastResultIsCorrect()
+    {
+      Assert.That(_results.Last().Page, Is.EqualTo("Wikipedia:Requests for page protection"));
+      Assert.That(_results.Last().Count, Is.EqualTo(17));
+    }
+
+    internal class QueryResult
+    {
+      public string Page { get; set; }
+
+      public int Count { get; set; }
+    }
+  }
+}
