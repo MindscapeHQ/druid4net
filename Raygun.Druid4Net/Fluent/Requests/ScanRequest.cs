11b1daca67736aba63f650a427bc3d8d1a8c2c56
Jeremy Norman <jnorman@raygun.io>
2019-03-03 08:14:22 +1000

    Add scan query descriptor and request objects

---
+++ Raygun.Druid4Net/Fluent/Requests/ScanRequest.cs
@@ -0,0 +1,14 @@
+﻿namespace Raygun.Druid4Net
+{
+  internal class ScanRequest : IDruidRequest<ScanRequestData>
+  {
+    public ScanRequestData RequestData { get; private set; }
+
+    public void Build<T>(T queryDescriptor) where T : IScanQueryDescriptor
+    {
+      var qd = queryDescriptor as ScanQueryDescriptor;
+
+      RequestData = new ScanRequestData(qd.DataSourceValue, qd.IntervalsValue, qd.FilterValue, qd.ContextValue, qd.ColumnsValue, qd.ResultFormatValue, qd.LimitValue, qd.BatchSizeValue);
+    }
+  }
+}
