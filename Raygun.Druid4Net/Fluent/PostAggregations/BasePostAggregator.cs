5a14f3a8cee086fefef377a100c5d88e5fe4a7b1
Jeremy Norman <jnorman@raygun.io>
2019-08-12 22:02:16 +1200

    Add new post aggregators for greatest/least

---
+++ Raygun.Druid4Net/Fluent/PostAggregations/BasePostAggregator.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+namespace Raygun.Druid4Net
+{
+  public abstract class BasePostAggregator : IPostAggregationSpec
+  {
+    public abstract string Type { get; }
+
+    public string Name { get; }
+    
+    public IEnumerable<IPostAggregationSpec> Fields { get; }
+    
+    protected BasePostAggregator(string name, IEnumerable<IPostAggregationSpec> fields)
+    {
+      Name = name;
+      Fields = fields;
+    }
+    
+    protected BasePostAggregator(string name, params IPostAggregationSpec[] fields)
+    {
+      Name = name;
+      Fields = fields;
+    }
+  }
+}
---
+++ Raygun.Druid4Net/Fluent/PostAggregations/DoubleLeastPostAggregator.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace Raygun.Druid4Net
+{
+  public class DoubleLeastPostAggregator : BasePostAggregator
+  {
+    public override string Type => "doubleLeast";
+
+    public DoubleLeastPostAggregator(string name, IEnumerable<IPostAggregationSpec> fields) 
+      : base(name, fields)
+    {
+    }
+
+    public DoubleLeastPostAggregator(string name, params IPostAggregationSpec[] fields) 
+      : base(name, fields)
+    {
+    }
+  }
+}
