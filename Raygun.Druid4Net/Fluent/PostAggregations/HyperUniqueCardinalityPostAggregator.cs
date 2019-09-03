61225140ebd6e66629df9705452688dd472cb39b
Jeremy Norman <jnorman@raygun.io>
2019-09-03 20:38:23 +1000

    Add hyperunique, javascript and constant post aggregators

---
+++ Raygun.Druid4Net/Fluent/PostAggregations/HyperUniqueCardinalityPostAggregator.cs
@@ -0,0 +1,17 @@
+﻿namespace Raygun.Druid4Net
+{
+  public class HyperUniqueCardinalityPostAggregator : IPostAggregationSpec
+  {
+    public virtual string Type => "hyperUniqueCardinality";
+
+    public string Name { get; }
+
+    public string FieldName { get; }
+
+    public HyperUniqueCardinalityPostAggregator(string name, string fieldName)
+    {
+      Name = name;
+      FieldName = fieldName;
+    }
+  }
+}
