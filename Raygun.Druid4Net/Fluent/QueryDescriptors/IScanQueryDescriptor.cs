d579eb073e32f815457288b887c8117644541418
PZawisza <piotr.zawisza92@gmail.com>
2020-09-10 13:18:15 +0200

    Handle Order parameter in Scan query

--- Raygun.Druid4Net/Fluent/QueryDescriptors/IScanQueryDescriptor.cs
+++ Raygun.Druid4Net/Fluent/QueryDescriptors/IScanQueryDescriptor.cs
@@ -24,6 +24,8 @@ namespace Raygun.Druid4Net
     IScanQueryDescriptor BatchSize(int batchSize);
     
     IScanQueryDescriptor Limit(int limit);
+
+    IScanQueryDescriptor Order(OrderByDirection? order);
     
     IScanQueryDescriptor Context(int? timeout = null, long? maxScatterGatherBytes = null, int? priority = null, string queryId = null, bool? useCache = null, bool? populateCache = null, bool? bySegment = null, bool? finalize = null, string chunkPeriod = null, bool? serializeDateTimeAsLong = null, bool? serializeDateTimeAsLongInner = null);
 
