7a371c2a11654dbaf29be3e4ac6765665ef399de
Andrew Bridge <andrew.bridge@derivitec.com>
2021-03-15 18:58:29 +0000

    Feat: Add Interval string parser

--- Raygun.Druid4Net/Fluent/Intervals/Interval.cs
+++ Raygun.Druid4Net/Fluent/Intervals/Interval.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 
 namespace Raygun.Druid4Net
 {
@@ -16,6 +17,27 @@ namespace Raygun.Druid4Net
       {
         To = From;
       }
+    }
+
+    public static Interval Parse(string intervalString)
+    {
+      if (intervalString == null) throw new ArgumentNullException(nameof(intervalString));
+      var formatException = new FormatException("The string could not be parsed to an Interval.");
+      if (!intervalString.Contains("/")) throw formatException;
+      var dates = intervalString.Split('/');
+      if (dates.Length != 2) throw formatException;
+      DateTime fromDate;
+      DateTime toDate;
+      try
+      {
+        fromDate = DateTime.Parse(dates[0], null, DateTimeStyles.RoundtripKind);
+        toDate = DateTime.Parse(dates[1], null, DateTimeStyles.RoundtripKind);
+      }
+      catch
+      {
+        throw formatException;
+      }
+      return new Interval(fromDate, toDate);
     }
 
     public string ToInterval()
