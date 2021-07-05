d13e97bb203c87f40671f50acd5c738541f22476
Jeremy Norman <jnorman@raygun.io>
2021-07-05 14:07:34 +1200

    Add test data and ingestion spec

--- Raygun.Druid4Net.IntegrationTests/Queries/TestQueryBase.cs
+++ Raygun.Druid4Net.IntegrationTests/Queries/TestQueryBase.cs
@@ -5,8 +5,8 @@ namespace Raygun.Druid4Net.IntegrationTests.Queries
 {
   public abstract class TestQueryBase
   {
-    protected DateTime FromDate = new DateTime(2015, 9, 12, 0, 0, 0, DateTimeKind.Utc);
-    protected DateTime ToDate = new DateTime(2015, 9, 13, 0, 0, 0, DateTimeKind.Utc);
+    protected DateTime FromDate = new DateTime(2016, 6, 27, 0, 0, 0, DateTimeKind.Utc);
+    protected DateTime ToDate = new DateTime(2016, 6, 28, 0, 0, 0, DateTimeKind.Utc);
 
     protected IDruidClient DruidClient;
 
