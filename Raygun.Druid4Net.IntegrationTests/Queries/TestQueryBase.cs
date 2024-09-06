608e28f5328532b20782c08e95fdf5d146ebfc6f
Jared Scholz <jaredsc108@gmail.com>
2024-09-06 16:56:26 +1200

    Update Druid4Net.IntegrationTests to .NET 6

--- Raygun.Druid4Net.IntegrationTests/Queries/TestQueryBase.cs
+++ Raygun.Druid4Net.IntegrationTests/Queries/TestQueryBase.cs
@@ -1,23 +1,29 @@
 ﻿using System;
-using System.Configuration;
+using System.IO;
+using Microsoft.Extensions.Configuration;
 
-namespace Raygun.Druid4Net.IntegrationTests.Queries
+namespace Raygun.Druid4Net.IntegrationTests.Queries;
+
+public abstract class TestQueryBase
 {
-  public abstract class TestQueryBase
-  {
-    protected DateTime FromDate = new DateTime(2016, 6, 27, 0, 0, 0, DateTimeKind.Utc);
-    protected DateTime ToDate = new DateTime(2016, 6, 28, 0, 0, 0, DateTimeKind.Utc);
+    private static readonly IConfigurationRoot Configuration = new ConfigurationBuilder()
+                                                               .SetBasePath(Directory.GetCurrentDirectory())
+                                                               .AddJsonFile("appsettings.json")
+                                                               .Build();
 
-    protected IDruidClient DruidClient;
+    protected readonly IDruidClient DruidClient;
+
+    protected DateTime FromDate = new(2016, 6, 27, 0, 0, 0, DateTimeKind.Utc);
+    protected DateTime ToDate = new(2016, 6, 28, 0, 0, 0, DateTimeKind.Utc);
 
     protected TestQueryBase()
     {
-      var options = new ConfigurationOptions()
-      {
-        JsonSerializer = new JilSerializer(),
-        QueryApiBaseAddress = new Uri(ConfigurationManager.AppSettings["druid.broker.host"])
-      };
-      DruidClient = new DruidClient(options);
+        var options = new ConfigurationOptions
+        {
+            JsonSerializer = new JilSerializer(),
+            QueryApiBaseAddress = new Uri(Configuration["DruidBroker:Host"])
+        };
+
+        DruidClient = new DruidClient(options);
     }
-  }
 }
