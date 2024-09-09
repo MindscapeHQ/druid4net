3d79b70a7cf40faaf852fa4012a44913818017c9
Jared Scholz <jaredsc108@gmail.com>
2024-09-10 10:01:38 +1200

    Clean up
    Delete JilSerializer
    Delete AssemblyInfos
    Fix formatting

--- Raygun.Druid4Net.IntegrationTests/Queries/TestQueryBase.cs
+++ Raygun.Druid4Net.IntegrationTests/Queries/TestQueryBase.cs
@@ -6,24 +6,24 @@ namespace Raygun.Druid4Net.IntegrationTests.Queries;
 
 public abstract class TestQueryBase
 {
-    private static readonly IConfigurationRoot Configuration = new ConfigurationBuilder()
-                                                               .SetBasePath(Directory.GetCurrentDirectory())
-                                                               .AddJsonFile("appsettings.json")
-                                                               .Build();
+  private static readonly IConfigurationRoot Configuration = new ConfigurationBuilder()
+                                                             .SetBasePath(Directory.GetCurrentDirectory())
+                                                             .AddJsonFile("appsettings.json")
+                                                             .Build();
 
-    protected readonly IDruidClient DruidClient;
+  protected readonly IDruidClient DruidClient;
 
-    protected DateTime FromDate = new(2016, 6, 27, 0, 0, 0, DateTimeKind.Utc);
-    protected DateTime ToDate = new(2016, 6, 28, 0, 0, 0, DateTimeKind.Utc);
+  protected DateTime FromDate = new(2016, 6, 27, 0, 0, 0, DateTimeKind.Utc);
+  protected DateTime ToDate = new(2016, 6, 28, 0, 0, 0, DateTimeKind.Utc);
 
-    protected TestQueryBase()
+  protected TestQueryBase()
+  {
+    var options = new ConfigurationOptions
     {
-        var options = new ConfigurationOptions
-        {
-            JsonSerializer = new JilSerializer(),
-            QueryApiBaseAddress = new Uri(Configuration["DruidBroker:Host"])
-        };
+      JsonSerializer = new NewtonsoftSerializer(),
+      QueryApiBaseAddress = new Uri(Configuration["DruidBroker:Host"])
+    };
 
-        DruidClient = new DruidClient(options);
-    }
+    DruidClient = new DruidClient(options);
+  }
 }
